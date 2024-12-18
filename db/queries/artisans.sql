5da7f4376c56a55d58f794b52b7e2ef406338dde
YASSER Korzane <ly_korzane@esi.dz>
2024-12-18 17:54:55 +0100

    add the artisans CRUD queries

--- db/queries/artisans.sql
+++ db/queries/artisans.sql
@@ -5,5 +5,43 @@ SELECT * FROM artisans;
 INSERT INTO artisans (username,email,phoneNumber, password) VALUES ($1, $2, $3,$4) RETURNING id, username;
 
 
+-- Update artisan name 3
+UPDATE artisans
+SET name = $1
+WHERE id = $2
+RETURNING *;
 
+-- Update artisan specialization 4
+UPDATE artisans
+SET specialization = $1
+WHERE id = $2
+RETURNING *;
 
+-- Update artisan contact_info 5
+UPDATE artisans
+SET contact_info = $1
+WHERE id = $2
+RETURNING *;
+
+-- Update artisan portfolio 6
+UPDATE artisans
+SET portfolio = $1
+WHERE id = $2
+RETURNING *;
+
+-- Update artisan certifications 7
+UPDATE artisans
+SET certifications = $1
+WHERE id = $2
+RETURNING *;
+
+-- Update artisan insurance_details 8
+UPDATE artisans
+SET insurance_details = $1
+WHERE id = $2
+RETURNING *;
+
+-- Delete an artisan 9
+DELETE FROM artisans
+WHERE id = $1
+RETURNING id;
