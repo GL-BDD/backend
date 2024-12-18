4c1c2264848f831829da73531e07f536022d7333
YASSER Korzane <ly_korzane@esi.dz>
2024-12-18 18:20:35 +0100

    add the artisans CRUD endpoints

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ const bd = require("./db/connections");
 // const artisansRoute = require("./routes/artisans");
 const authRoutes = require("./routes/auth");
 const clientRoutes = require("./routes/clients");
+const artisansRoute = require("./routes/artisans");
 
 app.use(express.json());
 // app.use("/api/artisans", artisansRoute);
 app.use("/api/clients", clientRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api/artisans", artisansRoute);
 
 app.get("/", async (req, res) => {
   const response = await bd.query("SELECT * FROM playing_with_neon;");
--- routes/artisans.js
+++ routes/artisans.js
@@ -1,11 +1,18 @@
+module.exports = router;
 const express = require("express");
 const {
   getArtisans,
+  getArtisanById,
   createArtisan,
+  deleteArtisan,
 } = require("../controllers/artisanController");
+const { authenticateToken } = require("../middleware/authMiddleware");
 const router = express.Router();
+exports.router = router;
 
 router.get("/", getArtisans);
+router.get("/:id", getArtisanById);
 router.post("/", createArtisan);
+router.delete("/", authenticateToken, deleteArtisan);
 
 module.exports = router;
