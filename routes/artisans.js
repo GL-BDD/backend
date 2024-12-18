af08841de0124988af090541aadacb1d8940f6ce
YASSER Korzane <ly_korzane@esi.dz>
2024-12-18 18:35:00 +0100

    add the validation for the artisan schema when creating and updating a new one

--- routes/artisans.js
+++ routes/artisans.js
@@ -1,18 +1,20 @@
-module.exports = router;
 const express = require("express");
 const {
   getArtisans,
   getArtisanById,
   createArtisan,
+  updateArtisan,
   deleteArtisan,
 } = require("../controllers/artisanController");
 const { authenticateToken } = require("../middleware/authMiddleware");
+const artisanSchema = require("../validations/aritsanValidation");
+const validateRequest = require("../middleware/validationMiddleware");
 const router = express.Router();
-exports.router = router;
 
 router.get("/", getArtisans);
 router.get("/:id", getArtisanById);
-router.post("/", createArtisan);
+router.post("/", artisanSchema.create, validateRequest, createArtisan);
+router.put("/", authenticateToken, artisanSchema.update, validateRequest, updateArtisan);
 router.delete("/", authenticateToken, deleteArtisan);
 
 module.exports = router;
